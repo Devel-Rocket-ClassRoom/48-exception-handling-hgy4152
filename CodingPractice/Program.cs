b793a68b53c2ce1fe7aaf34ec44b068b22bf47c7
Giyeong - Heo <hgy4152@gmail.com>
2026-03-13 16:45:49 +0900

    CodingPractice48
    예외처리 예제

--- CodingPractice/Program.cs
+++ CodingPractice/Program.cs
@@ -2,4 +2,34 @@ using System;
 using System.Collections.Generic;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+
+try
+{
+    int[] ints = { 1, 2, };
+    ints[100] = 1;
+}
+catch
+{
+    Console.WriteLine("에러가 발생했습니다.");
+}
+
+try
+{
+    int[] ints = { 1, 2, };
+    ints[100] = 1;
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"에외 발생: {ex}");
+}
+
+try
+{
+    int a = 3.14;
+}
+catch
+{
+    Console.WriteLine("에러가 발생했습니다.");
+}
+
---
+++ SafeCalculator/SafeCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class SafeCalculator
+{
+    
+    public int Divide(string num1, string num2)
+    {
+        int result = 0;
+        try
+        {
+            int d1 = int.Parse(num1);
+            int d2 = int.Parse(num2);
+
+            result = d1 / d2;
+            Console.WriteLine($"{d1} / {d2} = {result}");
+        }
+        catch(FormatException e)
+        {
+            Console.WriteLine("올바른 숫자를 입력하세요");
+        }
+        catch(DivideByZeroException e) 
+        {
+            Console.WriteLine("0으로 나눌 수 없습니다.");
+        }
+        finally
+        {
+            Console.WriteLine("계산기를 종료합니다");
+        }
+
+        return result;
+    }
+}
